65dca46530da8f7d1853d42258d73cf8f74366a5
Kaii-Kaii <khanghu93@gmail.com>
2025-04-25 13:01:50 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:qltncn/screens/HomePage.dart';
+import 'package:qltncn/screens/s_Login/DangNhap.dart';
 
 void main() {
-  runApp(const MyApp());
+  //runApp(const MyApp());
+  runApp(MyAppLogin());
+}
+
+
+
+class MyAppLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QL TNCN',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
